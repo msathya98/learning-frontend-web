7f4829b04395e5697612b8d607bcb8b1e53ed5b4
sathya-sjce <62427113+sathya-sjce@users.noreply.github.com>
2020-10-14 17:18:18 +0530

    Add files via upload

---
+++ JavaScipt/basics.js
@@ -0,0 +1,15 @@
+var sum= 4+5;
+var differnece= 4-5;
+alert("sum of 4 + 5 is");
+alert(sum);
+alert("differnece of 4 - 5 is");
+alert(differnece);
+
+var fav = "hello" + ", " + "world!";
+alert(fav);
+
+var foo= parseInt("34", 10); //convert to string to integer number 
+alert(foo);
+var joo = parseFloat("3.13", 10); // convert to string to float number
+alert(joo);
+
